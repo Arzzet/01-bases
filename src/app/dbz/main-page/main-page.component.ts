fd018f0e329fd65f3d82d781f4a3783f6ae02c66
Iwo <iwoskater3@hotmail.com>
2021-12-15 11:35:20 +0100

    add pj

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 9001
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 9002
+    }
+  ];
 
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: "",
+    poder: 0
+  }
+
+  agregar() {
+    if(this.nuevo.nombre.trim().length === 0) { return }
+    // if(typeof(this.nuevo.poder)!= 'number' ) { return }
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo= {
+      nombre: "",
+      poder: 0
+    }
   }
 
 }
